c4a4c1f1c7b27aec17723b28b5ab7cfe4370cf68
Santhosh <esanthosh@deloitte.com>
2021-08-29 20:32:32 +0530

    update method

--- src/main/java/com/azuresql/jpa/SpringbootAzureSqlApplication.java
+++ src/main/java/com/azuresql/jpa/SpringbootAzureSqlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,11 @@ public class SpringbootAzureSqlApplication {
 	
 	@PostMapping("/save_employee")
 	public Emplyeeobj addEmployee(@RequestBody Emplyeeobj emplyeeobj) {
-		System.out.println(emplyeeobj.getName());
+		return emplyeeRepo.save(emplyeeobj);
+	}
+	
+	@PutMapping("/update_employee")
+	public Emplyeeobj updateEmployee(@RequestBody Emplyeeobj emplyeeobj) {
 		return emplyeeRepo.save(emplyeeobj);
 	}
 	
