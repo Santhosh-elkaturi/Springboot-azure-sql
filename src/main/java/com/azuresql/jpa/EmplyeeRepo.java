6bbc6cbb78e0dbfea5177d7f7507a91912697697
Santhosh <esanthosh@deloitte.com>
2021-08-29 11:40:49 +0530

    code commit

---
+++ src/main/java/com/azuresql/jpa/EmplyeeRepo.java
@@ -0,0 +1,7 @@
+package com.azuresql.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmplyeeRepo extends JpaRepository<Emplyeeobj, Integer>{
+
+}
---
+++ src/main/java/com/azuresql/jpa/SpringbootAzureSqlApplication.java
@@ -0,0 +1,35 @@
+package com.azuresql.jpa;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootAzureSqlApplication {
+
+	@Autowired
+	private EmplyeeRepo emplyeeRepo;
+	
+	@PostMapping("/save_employee")
+	public Emplyeeobj addEmployee(@RequestBody Emplyeeobj emplyeeobj) {
+		System.out.println(emplyeeobj.getName());
+		return emplyeeRepo.save(emplyeeobj);
+	}
+	
+	@GetMapping("/getemployees")
+	public List<Emplyeeobj> getEmplyess() {
+		return emplyeeRepo.findAll();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAzureSqlApplication.class, args);
+	}
+
+}
